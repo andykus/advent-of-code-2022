3493bff4a95461d8e6bb2ded59c35d9e71a22dd8
andykus <andy.emilsson@gmail.com>
2022-12-06 13:18:21 +0100

    Refactor day 6

--- 6/6.js
+++ 6/6.js
@@ -3,17 +3,9 @@ import { promises as fs } from 'fs';
 const data = await fs.readFile('./input', 'utf-8');
 
 const findMarkerIndex = (markerSize) => {
-    const currentSegment = [];
     for (let i = 0; i < data.length; i++) {
-        if (currentSegment.length === markerSize) {
-            currentSegment.shift();
-        }
-
-        currentSegment.push(data[i]);
-        const isMarker = [...new Set(currentSegment)].length === markerSize;
-
-        if (isMarker) {
-            return i + 1;
+        if (new Set(data.slice(i, i + markerSize)).size === markerSize) {
+            return i + markerSize;
         }
     }
 };
